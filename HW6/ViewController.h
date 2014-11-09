7d740630e7231479872057fb735f145b0f1d0371
ferz <s5005a@gmail.com>
2014-11-09 16:49:17 +0300

    firs commit

---
+++ HW6/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HW6
+//
+//  Created by Administrator lepshi on 06.11.14.
+//  Copyright (c) 2014 Administrator lepshi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HW6/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HW6
+//
+//  Created by Administrator lepshi on 06.11.14.
+//  Copyright (c) 2014 Administrator lepshi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
